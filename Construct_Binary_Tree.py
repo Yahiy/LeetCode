9650e739483bec98addd21e583c165e46fee7a08
Yahiy <yuanhuan9412@163.com>
2019-05-08 14:43:48 +0800

    Update Construct_Binary_Tree.py
    Given inorder and postorder traversal of a tree, construct the binary tree.

--- Construct_Binary_Tree.py
+++ Construct_Binary_Tree.py
@@ -1,5 +1,5 @@
 """
-Given preorder and inorder traversal of a tree, construct the binary tree.
+105.Given preorder and inorder traversal of a tree, construct the binary tree.
 
 Note:
 You may assume that duplicates do not exist in the tree.
@@ -45,3 +45,52 @@ class Solution(object):
             root.left = self.buildTree(lp,li)
             root.right = self.buildTree(rp,ri)
         return root
+    
+"""
+106.Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not len(postorder):
+            return None
+        if len(postorder) == 1:
+            root = TreeNode(postorder[-1])
+            return root
+        else:
+            root = TreeNode(postorder[-1])
+            i = inorder.index(postorder[-1])
+            li = inorder[:i]
+            ri = inorder[i+1:]
+            lp = postorder[:i]
+            rp = postorder[i:-1]
+            root.left = self.buildTree(li, lp)
+            root.right = self.buildTree(ri, rp)
+        return root
+        
