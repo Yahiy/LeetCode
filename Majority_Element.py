a67860d125d926b0b89028255839f347a1f12864
Yahiy <yuanhuan9412@163.com>
2019-05-16 13:52:40 +0800

    Create Majority_Element.py
    Moore Voting find majority elements

---
+++ Majority_Element.py
@@ -0,0 +1,51 @@
+"""
+ Moore Voting, Boyer-Moore Majority Vote Algorithm
+169. Majority Element
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+"""
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return None
+        result, times = nums[0], 0
+        for i in nums:
+            if times == 0:
+                result = i
+                times = 1
+            elif result == i:
+                times+=1
+            else:
+                times-=1
+        return result
+
+"""
+229. Majority Element II
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+Note: The algorithm should run in linear time and in O(1) space.
+
+Example 1:
+
+Input: [3,2,3]
+Output: [3]
+Example 2:
+
+Input: [1,1,1,3,3,2,2,2]
+Output: [1,2]
+"""
