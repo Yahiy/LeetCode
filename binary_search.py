e83070dc44e46a038653448b182b259e8271199d
Yahiy <yuanhuan9412@163.com>
2019-05-27 15:13:51 +0800

    Update binary_search.py
     Find First and Last Position of Element in Sorted Array

--- binary_search.py
+++ binary_search.py
@@ -243,8 +243,72 @@ class Solution(object):
         return None
             
       
+"""
+34. Find First and Last Position of Element in Sorted Array
+Medium
+Given an array of integers nums sorted in ascending order, 
+find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
 
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+"""
 
-
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        first = self.findFirst(nums,target,n)
+        if first==-1 or first == n-1:
+            return [first,first]
+        least = self.findLeast(nums,target,n, first)
+        return [first,least]
+        
+        
+    def findFirst(self,nums,t,n):
+        s,e = 0,n-1
+        
+        while(s<=e):
+            m = (s+e)//2
+            mid = nums[m]
+            if mid == t:
+                if m>0 and nums[m-1]!= t or m==0:
+                    return m
+                else:
+                    e = m-1
+            elif mid>t:
+                e = m-1
+            else:
+                s = m+1
+        return -1
+    
+    def findLeast(self,nums,t,n,f):
+        s,e = f,n-1
+        
+        while(s<=e):
+            m = (s+e)//2
+            mid = nums[m]
+            if mid == t:
+                if m<n-1 and nums[m+1]!= t or m==n-1:
+                    return m
+                else:
+                    s = m+1
+            elif mid>t:
+                e = m-1
+            else:
+                s = m+1
+    
+    
+        
+        
 
 
